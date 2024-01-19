7ce484693c4db9072538a04d4f35aa07a340237f
shyam2323232323232 <131328505+shyam2323232323232@users.noreply.github.com>
2024-01-19 23:38:00 +0530

    final

--- quiz.js
+++ quiz.js
@@ -49,99 +49,201 @@ let questions=[{
 const Question=document.querySelector('#question')
 const ansewers=document.querySelector('#ansewer-buttons')
 const next=document.querySelector('#next-button')
-//
 
 let currentIndex=0
 let score=0
 init()
-function  init(){      
-     currentIndex=0;
-    score=0; 
-    next.innerHTML='N E X T'
-    showquestion() 
-
+function init(){
+    currentIndex=0
+    score=0
+    next.innerHTML="N E X T"
+    startgame()
+    Question.style.fontSize='20px'
+    Question.style.textAlign ='left'
 }
-
-function showquestion() { 
-let  currentQuestion=questions[currentIndex]  
-reset()
-let score=0
-
-
-let questionNo=1+currentIndex;
-Question.innerHTML=questionNo+". "+currentQuestion.question
-
-currentQuestion.answer.forEach(answer => {
-        let  ansBtn=document.createElement('button')
-        ansBtn.innerHTML=answer.text
-        ansBtn.classList.add('btn')
-        ansewers.appendChild(ansBtn)  
+function startgame(){
+    reset()
+    let ques=questions[currentIndex]
+    let number=currentIndex+1
+    Question.innerHTML=number+". "+ques.question
+    ques.answer.forEach(answer=>{
+        let ans=document.createElement('button')
+        ansewers.appendChild(ans)
+        ans.classList.add('btn')
+        ans.innerHTML=answer.text
         if(answer.correct){
-            ansBtn.dataset.correct=answer.correct
+            ans.dataset.correct=answer.correct            
         }
-            ansBtn.addEventListener('click',correction
-        ) 
-
-});
-
-   
+        ans.addEventListener('click',run)
+    })
+    
 }
-function correction(e){
-     const selectedBtn=e.target
-    //  console.log(selectedBtn);
-    const correct=selectedBtn.dataset.correct
-  if(correct=="true"){
-    // console.log('true');   
-     selectedBtn.classList.add('correct')
-    score++
-  }else{
-    selectedBtn.classList.add('wrong')
-  }
-  Array.from(ansewers.children).forEach(button=>{
-    if(button.dataset.correct=="true"){
-        button.classList.add('correct')
-        next.style.display='block'
+function run(e){
+ 
+let k=e.target 
+let correction=k.dataset.correct
+
+if(correction=="true"){
+score++
+    k.classList.add('correct')
+}else{
+    k.classList.add('wrong')
+}
+Array.from(ansewers.children).forEach(buttons=>{
+    if(buttons.dataset.correct=="true"){      
+        buttons.classList.add('correct')
+        
     }
-    button.disabled=true
-    button.style.pointerEvents= "none";
-  })
+    next.style.display='block'
+    buttons.style.pointerEvents='none' 
+    buttons.disabled="true"
+})
+   
 
-
+// console.log(ansewers.children);
 }
 next.addEventListener('click',()=>{
     if(currentIndex<questions.length){
-        showNext()         
-    }else{
-        init()
-    }
+        ji()
+        }else{
+         init()
+        }
 })
 
-function showNext(){
-    currentIndex+=1
+function ji(){
+    currentIndex++
     if(currentIndex<questions.length){
-        showquestion() 
-      
+        startgame()
     }else{
-     showscore()
+    final()
     }
- 
-
 }
-function showscore(){
-    Question.innerHTML=`your score is ${score}`
-    next.innerHTML="PLAY AGAIN"
+function final(){
     reset()
-    next.style.display='block'
-    
+    next.innerHTML="play again"
+    next.style.display="block"
+    Question.style.fontSize='50px'
+    Question.style.textAlign ='center'
+    Question.innerHTML=`your score ${score}`
 }
 function reset(){
-    next.style.display='none'
-    // console.log(ansewers.firstChild);
-    while(ansewers.firstChild){  
-        console.log(ansewers.firstChild);     
+    next.style.display="none"
+    while(ansewers.firstChild){
         ansewers.removeChild(ansewers.firstChild)
-       
     }
-
 }
-// console.log(ansewers.firstChild);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let currentIndex=0
+// let score=0
+// init()
+// function  init(){      
+//      currentIndex=0;
+//     score=0; 
+//     next.innerHTML='N E X T'
+//     showquestion() 
+
+// }
+
+// function showquestion() { 
+// let  currentQuestion=questions[currentIndex]  
+// reset()
+// let score=0
+
+
+// let questionNo=1+currentIndex;
+// Question.innerHTML=questionNo+". "+currentQuestion.question
+
+// currentQuestion.answer.forEach(answer => {
+//         let  ansBtn=document.createElement('button')
+//         ansBtn.innerHTML=answer.text
+//         ansBtn.classList.add('btn')
+//         ansewers.appendChild(ansBtn)  
+//         if(answer.correct){
+//             ansBtn.dataset.correct=answer.correct
+//         }
+//             ansBtn.addEventListener('click',correction
+//         ) 
+
+// });
+
+   
+// }
+// function correction(e){
+//      const selectedBtn=e.target
+//     //  console.log(selectedBtn);
+//     const correct=selectedBtn.dataset.correct
+//   if(correct=="true"){
+//     // console.log('true');   
+//      selectedBtn.classList.add('correct')
+//     score++
+//   }else{
+//     selectedBtn.classList.add('wrong')
+//   }
+//   Array.from(ansewers.children).forEach(button=>{
+//     if(button.dataset.correct=="true"){
+//         button.classList.add('correct')
+//         next.style.display='block'
+//     }
+//     button.disabled=true
+//     button.style.pointerEvents= "none";
+//   })
+
+
+// }
+// next.addEventListener('click',()=>{
+//     if(currentIndex<questions.length){
+//         showNext()         
+//     }else{
+//         init()
+//     }
+// })
+
+// function showNext(){
+//     currentIndex+=1
+//     if(currentIndex<questions.length){
+//         showquestion() 
+      
+//     }else{
+//      showscore()
+//     }
+ 
+
+// }
+// function showscore(){
+//     Question.innerHTML=`your score is ${score}`
+//     next.innerHTML="PLAY AGAIN"
+//     reset()
+//     next.style.display='block'
+    
+// }
+// function reset(){
+//     next.style.display='none'
+//     // console.log(ansewers.firstChild);
+//     while(ansewers.firstChild){  
+//         console.log(ansewers.firstChild);     
+//         ansewers.removeChild(ansewers.firstChild)
+       
+//     }
+
+// }
+// // console.log(ansewers.firstChild);
